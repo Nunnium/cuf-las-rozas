440c3dcfca3ab59d57efaf08cf534920851de6b0
Nuño <marincabello@gmail.com>
2024-12-04 12:36:39 +0100

    docs actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from "./components/cabecera/cabecera.component";
-import { InicioComponent } from "./components/paginas/inicio/inicio.component";
 import { BarraLateralComponent } from "./components/barra-lateral/barra-lateral/barra-lateral.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent, InicioComponent, BarraLateralComponent, FooterComponent],
+  imports: [RouterOutlet, CabeceraComponent, BarraLateralComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
